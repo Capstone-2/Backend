aaee05e703a8ce061c560440e84c8d486ca06069
Alvin0820 <your-email@example.com>
2026-07-28 15:23:42 -0400

    Add user model

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const User = require("./Users")
+const db = require("../db")// Database connection
+/* Call the files location so that would be our database connection,
+
+
+*/
+
+
+
+// Association
+
+
+
+
+    module.exports = {
+        db,
+        User,
+    }
