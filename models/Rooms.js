9cba457ac50b77cb50baa5fa6ea6424aebb779e4
Alvin0820 <your-email@example.com>
2026-07-28 16:36:33 -0400

    Add Session Model

--- models/Rooms.js
+++ models/Rooms.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 const db = require("../db");
 
-const RoomsModel = db.define("room", {
+const RoomsModel = db.define("Room", {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
