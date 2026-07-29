5759ae90316338426312459d1fca0e6bdcbca028
Alvin0820 <your-email@example.com>
2026-07-29 12:51:05 -0400

    made some changes

--- models/Messages.js
+++ models/Messages.js
@@ -2,14 +2,6 @@ const { DataTypes } = require("sequelize");
 const db = require("../db");
 
 const Messages = db.define("Message", {
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  room_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
   text: {
     type: DataTypes.TEXT,
     allowNull: false,
